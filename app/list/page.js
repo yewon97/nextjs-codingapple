72a62440d869982a5e7c63c9df7c3768a4b5128d
yewon97 <dpdnjs9936@gmail.com>
2023-11-21 23:26:56 +0900

    /list 페이지 만듬

---
+++ app/list/page.js
@@ -0,0 +1,13 @@
+export default function ListPage() {
+  return (
+    <div>
+      <h2 className="title">Products</h2>
+      <div className="food">
+        <h4>상품명 $40</h4>
+      </div>
+      <div className="food">
+        <h4>상품명 $40</h4>
+      </div>
+    </div>
+  );
+}
