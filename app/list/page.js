9cf3f5f19a8db8944bc72380db25917c6964e9e5
yewon97 <dpdnjs9936@gmail.com>
2023-11-21 23:44:01 +0900

    map 써보기

--- app/list/page.js
+++ app/list/page.js
@@ -1,13 +1,15 @@
 export default function ListPage() {
+  let product = ['tomatoes', 'pasta', 'coconut'];
   return (
     <div>
       <h2 className="title">Products</h2>
-      <div className="food">
-        <h4>상품명 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품명 $40</h4>
-      </div>
+      {product.map((food, idx) => {
+        return (
+          <div className="food" key={idx}>
+            <h4>{food} $40</h4>
+          </div>
+        );
+      })}
     </div>
   );
 }
