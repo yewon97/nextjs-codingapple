b76fe52f34776fbb74114c90a0e9aa19bcc5d560
yewon97 <dpdnjs9936@gmail.com>
2023-11-21 23:27:48 +0900

    layout에 nav 빼기

--- app/page.js
+++ app/page.js
@@ -4,10 +4,6 @@ export default function Home() {
   let name = 'jeon';
   return (
     <div>
-      <div className="navbar">
-        <Link href="/">home</Link>
-        <Link href="/list">list페이지</Link>
-      </div>
       <h4 className="title">애플후레시</h4>
       <p className="title-sub">by dev {name}</p>
     </div>
