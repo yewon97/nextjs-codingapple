3c82bac4e8c7b554cb098d484ccfdd24aa5e7d47
yewon97 <dpdnjs9936@gmail.com>
2023-11-21 23:54:57 +0900

    사진 추가, cart 페이지 생성

--- app/list/page.js
+++ app/list/page.js
@@ -1,11 +1,14 @@
+import Image from 'next/image';
+
 export default function ListPage() {
   let product = ['tomatoes', 'pasta', 'coconut'];
   return (
     <div>
       <h2 className="title">Products</h2>
-      {product.map((food, idx) => {
+      {product.map((food, i) => {
         return (
-          <div className="food" key={idx}>
+          <div className="food" key={i}>
+            <img src={`/food${i}.png`} alt={food} className="food-img" />
             <h4>{food} $40</h4>
           </div>
         );
