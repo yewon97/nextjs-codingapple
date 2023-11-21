f6f2999c21ae0949917d1da0e5db024e41002ff2
yewon97 <dpdnjs9936@gmail.com>
2023-11-21 23:20:53 +0900

    메인 페이지 살짝 만들어보기

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
-import Image from 'next/image';
-import styles from './page.module.css';
-
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <h4 className="title">애플후레시</h4>
+      <p className="title-sub">by dev kim</p>
+    </div>
+  );
 }
