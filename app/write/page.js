ace8d7fba448178ecccdb9b84a2497ec2cf396ad
yewon97 <dpdnjs9936@gmail.com>
2023-11-23 00:13:01 +0900

    에러처리 추가함

--- app/write/page.js
+++ app/write/page.js
@@ -5,10 +5,10 @@ export default function WritePage() {
       <form action="/api/post/new" method="POST">
         <fieldset>
           <legend>글작성</legend>
-          <input name="title" placeholder="글제목" />
-          <input name="content" placeholder="글내용" />
-          <button type="submit">전송</button>
+          <input type="text" name="title" placeholder="글제목" required />
+          <input type="text" name="content" placeholder="글내용" required />
         </fieldset>
+        <button type="submit">전송</button>
       </form>
     </div>
   );
--- pages/api/post/new.js
+++ pages/api/post/new.js
@@ -2,9 +2,16 @@ import { connectDB } from '@/util/database';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    let db = (await connectDB).db('forum');
-    let result = await db.collection('post').insertOne(req.body);
-    // console.log('result: ', result);
-    return res.redirect(302, '/list');
+    if (req.body.title == '') {
+      return 응답.status(500).json('제목써라');
+    }
+    try {
+      let db = (await connectDB).db('forum');
+      let result = await db.collection('post').insertOne(req.body);
+      // console.log('result: ', result);
+      return res.redirect(302, '/list');
+    } catch (error) {
+      throw error;
+    }
   }
 }
