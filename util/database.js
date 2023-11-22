37bb90a72b4c14b1925eef211a232a0ed5836c68
yewon97 <dpdnjs9936@gmail.com>
2023-11-22 23:45:45 +0900

    useNewUrlParser: true 안쓰는거라 지움

--- util/database.js
+++ util/database.js
@@ -4,16 +4,16 @@ dotenv.config();
 
 const url = `mongodb+srv://${process.env.DB_DATABASE}:${process.env.DB_PASSWORD}@cluster0.ehrl3hp.mongodb.net/?retryWrites=true&w=majority`;
 
-const options = { useNewUrlParser: true };
+// const options = { useNewUrlParser: true };
 
 let connectDB;
 
 if (process.env.NODE_ENV === 'development') {
   if (!global._mongo) {
-    global._mongo = new MongoClient(url, options).connect();
+    global._mongo = new MongoClient(url).connect();
   }
   connectDB = global._mongo;
 } else {
-  connectDB = new MongoClient(url, options).connect();
+  connectDB = new MongoClient(url).connect();
 }
 export { connectDB };
