f0c1bf17ac598a04487fe99e8b2762d2ad38b64f
yewon97 <dpdnjs9936@gmail.com>
2023-11-24 00:02:21 +0900

    dynamic 페이지로 만들기

--- app/list/page.js
+++ app/list/page.js
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import DetailLink from './DetailLink';
 import ListItem from './ListItem';
 
+export const dynamic = 'force-dynamic';
+
 export default async function ListPage() {
   const client = await connectDB;
   const db = client.db('forum');
