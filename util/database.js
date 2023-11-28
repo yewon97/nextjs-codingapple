446f6683b468ee763949fd4453b81f570bbeba6c
yewon97 <dpdnjs9936@gmail.com>
2023-11-28 22:47:22 +0900

    forum 데이터베이스에 유저 정보 저장하게 수정

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const url = `mongodb+srv://${process.env.DB_DATABASE}:${process.env.DB_PASSWORD}@cluster0.ehrl3hp.mongodb.net/?retryWrites=true&w=majority`;
+const url = `mongodb+srv://${process.env.DB_DATABASE}:${process.env.DB_PASSWORD}@cluster0.ehrl3hp.mongodb.net/forum?retryWrites=true&w=majority`;
 
 // const options = { useNewUrlParser: true };
 
