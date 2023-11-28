8fb35b8362b3df9cc55e5326e96017d10229d19e
yewon97 <dpdnjs9936@gmail.com>
2023-11-28 23:40:43 +0900

    아이디 비번 로그인 페이지 작업

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,6 +3,9 @@ import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import dotenv from 'dotenv';
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import bcrypt from 'bcrypt';
+
 dotenv.config();
 
 export const authOptions = {
@@ -11,7 +14,63 @@ export const authOptions = {
       clientId: process.env.GITHUB_AUTH_ID,
       clientSecret: process.env.GITHUB_AUTH_PASSWORD,
     }),
+    CredentialsProvider({
+      //1. 로그인페이지 폼 자동생성해주는 코드
+      name: 'credentials',
+      credentials: {
+        email: { label: 'email', type: 'text' },
+        password: { label: 'password', type: 'password' },
+      },
+
+      //2. 로그인요청시 실행되는코드
+      //직접 DB에서 아이디,비번 비교하고
+      //아이디,비번 맞으면 return 결과, 틀리면 return null 해야함
+      async authorize(credentials) {
+        let db = (await connectDB).db('forum');
+        let user = await db
+          .collection('user_cred')
+          .findOne({ email: credentials.email });
+        if (!user) {
+          alert('해당 이메일은 없음');
+          return null;
+        }
+        const pwcheck = await bcrypt.compare(
+          credentials.password,
+          user.password,
+        );
+        if (!pwcheck) {
+          alert('비번틀림');
+          return null;
+        }
+        return user;
+      },
+    }),
   ],
+
+  session: {
+    strategy: 'jwt',
+    maxAge: 30 * 24 * 60 * 60, //30일 // 1일 24 * 60 * 60
+  },
+
+  callbacks: {
+    //4. jwt 만들 때 실행되는 코드
+    //user변수는 DB의 유저정보담겨있고 token.user에 뭐 저장하면 jwt에 들어갑니다.
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.user = {};
+        token.user.name = user.name;
+        token.user.email = user.email;
+        // role도 보낼 수 있음
+      }
+      return token;
+    },
+    //5. 유저 세션이 조회될 때 마다 실행되는 코드
+    session: async ({ session, token }) => {
+      session.user = token.user;
+      return session;
+    },
+  },
+
   secret: process.env.SECRETKEY,
   adapter: MongoDBAdapter(connectDB), // redis써도됨
 };
