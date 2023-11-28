92d1f1bf1a0bef1769333ceb6edcef96653ca046
yewon97 <dpdnjs9936@gmail.com>
2023-11-29 00:20:42 +0900

    write 페이지는 로그인 한 사람만 입장 가능

--- app/write/page.js
+++ app/write/page.js
@@ -1,21 +1,30 @@
-export default function WritePage() {
-  return (
-    <div>
-      <h4>글작성</h4>
-      <form action="/api/post/new" method="POST">
-        <fieldset>
-          <legend>글작성</legend>
-          <input
-            type="text"
-            name="title"
-            placeholder="글제목"
-            required
-            autoFocus
-          />
-          <input type="text" name="content" placeholder="글내용" required />
-          <button type="submit">전송</button>
-        </fieldset>
-      </form>
-    </div>
-  );
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+
+export default async function WritePage() {
+  const session = await getServerSession(authOptions);
+  if (session == null) {
+    return redirect('/api/auth/signin');
+  } else {
+    return (
+      <div>
+        <h4>글작성</h4>
+        <form action="/api/post/new" method="POST">
+          <fieldset>
+            <legend>글작성</legend>
+            <input
+              type="text"
+              name="title"
+              placeholder="글제목"
+              required
+              autoFocus
+            />
+            <input type="text" name="content" placeholder="글내용" required />
+            <button type="submit">전송</button>
+          </fieldset>
+        </form>
+      </div>
+    );
+  }
 }
