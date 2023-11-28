25538bd11abe7d555bc57169b9d73fb36bdb2fe8
yewon97 <dpdnjs9936@gmail.com>
2023-11-28 23:32:37 +0900

    bcrypt 사용해서 비밀번호 암호화

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,6 +1,17 @@
+import { connectDB } from '@/util/database';
+import bcrypt from 'bcrypt';
+
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    let db = (await connectDB).db('forum');
-    let result = await db.collection('user_cred').insertOne(req.body);
+    const hashPw = await bcrypt.hash(req.body.password, 10);
+    req.body.password = hashPw;
+
+    try {
+      let db = (await connectDB).db('forum');
+      let result = await db.collection('user_cred').insertOne(req.body);
+      res.status(200).json('가입성공');
+    } catch (error) {
+      throw error;
+    }
   }
 }
